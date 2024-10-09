69ece3b332f291c851832c8ea988103bee903bb6
Mateusz <msikorski@griddynamics.com>
2024-10-09 09:18:38 +0200

    add initial java spring projects.

---
+++ kafka-publisher/src/main/java/com/github/mat_sik/kafka_publisher/KafkaPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.github.mat_sik.kafka_publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaPublisherApplication.class, args);
+	}
+
+}
---
+++ kafka-publisher/src/test/java/com/github/mat_sik/kafka_publisher/KafkaPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mat_sik.kafka_publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
